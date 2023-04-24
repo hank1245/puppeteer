f2bc2985f534a0c43040bd0b013797c240fafe76
Hank Kim <hank29206880@gmail.com>
2023-04-24 15:49:33 +0900

    browser automation

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ const puppeteer = require("puppeteer");
 const fs = require("fs");
 
 async function run() {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
-  await page.goto("https://www.traversymedia.com");
+  page.setViewport({
+    width: 1024,
+    height: 980,
+  });
+  //   await page.goto("https://www.traversymedia.com");
 
   //   await page.screenshot({ path: "naver.png", fullPage: true });
   //   await page.pdf({ path: "naver.pdf", format: 'A4' });
@@ -21,20 +25,25 @@ async function run() {
   //       url: el.querySelector(".card-footer a").href,
   //     }))
   //   );
-  const courses = await page.$$eval("#cscourses .card", (elements) =>
-    elements.map((el) => ({
-      title: el.querySelector(".card-body h3").innerText,
-      level: el.querySelector(".card-body .level").innerText,
-      url: el.querySelector(".card-footer a").href,
-    }))
-  );
-  console.log(courses);
-  fs.writeFile("courses.json", JSON.stringify(courses), (err) => {
-    if (err) throw err;
-    console.log("File saved");
-  });
+  //   const courses = await page.$$eval("#cscourses .card", (elements) =>
+  //     elements.map((el) => ({
+  //       title: el.querySelector(".card-body h3").innerText,
+  //       level: el.querySelector(".card-body .level").innerText,
+  //       url: el.querySelector(".card-footer a").href,
+  //     }))
+  //   );
 
-  await browser.close();
+  //   fs.writeFile("courses.json", JSON.stringify(courses), (err) => {
+  //     if (err) throw err;
+  //     console.log("File saved");
+  //   });
+
+  await page.goto("https://quotes.toscrape.com");
+  await page.click('a[href="/login"]');
+  await page.type("#username", "PedroTech", { delay: 100 });
+  await page.type("#password", "password", { delay: 100 });
+  await page.click('input[value="Login"]');
+  //   await browser.close();
 }
 
 run();
